e6ee4630f0058e8e36dca81ec8af70a7bd9242a7
Nnamdi <nnamdi.b@gmail.com>
2023-07-13 20:11:09 +0200

    created a service and outputs

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
+  @Output() submitted = new EventEmitter<string>();
+  term = '';
 
+  onInput(event: Event) {
+    this.term = (event.target as HTMLInputElement).value;
+  }
+
+  onFormSubmit(event: any) {
+    event?.preventDefault();
+    this.submitted.emit(this.term);
+  }
 }
